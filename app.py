946726db5dbee5b7c7ab3f145b146d26955f0f53
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-02 08:54:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import listas as lst
+import datetime
 
-st.write("teste")
+hoje['data'] = datatime.datetime.date()
+hoje['hora'] = datatime.datetime.time()
 
 with st.sidebar:
 	with st.expander('usuario',expanded=True):
+		cols = st.columns([1,1])
+		cols[0].text(hoje['data'])
+		cols[1].text(hoje['hora'])
+
 		st.selectbox("usuarios",lst.usuarios,key='usuario')
 		st.write(st.session_state['usuario'])
 
