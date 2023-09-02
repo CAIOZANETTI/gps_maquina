4ae4bde974103ebd7cb32eb8b3e1b51fdeb4e835
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-02 09:48:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,15 +21,37 @@ with st.sidebar:
 		st.write(st.session_state['usuario'])
 
 caminho =caminhos.tabelas['jcb_relatorio'] 
-st.write(caminho)
+#st.write(caminho)
 
 df = extrair.gsheet_to_df(
 	id = caminho['id'],
 	tabela=caminho['tabela'],
 	testar=False)
 
-st.dataframe(df)
+st.write('transformação da tabela')
+def df_bronze(coluna:str,df:pd.DataFrame)->pd.DataFrame:
+	df[['dia', 'mes', 'ano', 'hora','minuto']] = df[coluna].str.split(' |/:', expand=True)
+
+	# Convert columns to the desired data types
+	df['dia'] = df['dia'].astype(int)
+	df['mes'] = df['mes'].astype(int)
+	df['ano'] = df['ano'].astype(int)
+	df['hora'] = df['hora'].astype(int)
+	df['minuto'] = df['minuto'].astype(int)
+
+	return df
 
 
 
+with st.expander('raw',expanded=True):
+	st.dataframe(df)
 
+with st.expander('bronze',expanded=False):
+	df1 = df_bronze(coluna='Iniciar',df=df)
+	st.dataframe(df1)
+
+with st.expander('silver',expanded=False):
+	st.dataframe(df)
+
+with st.expander('gold',expanded=False):
+	st.dataframe(df)
