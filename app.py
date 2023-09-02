0daa25d09237f2117d220ea0694d541c6ada0a9a
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-02 09:32:23 -0300

    criando modulos

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import streamlit as st
-import listas as lst
 import datetime
+import streamlit as st
+
+#modulos
+import listas as lst
+import extrair
+import caminhos
 
 hoje={}
 hoje['datetime'] = datetime.datetime.now()
@@ -8,13 +12,24 @@ hoje['data'] = hoje['datetime'].date()
 hoje['hora'] = hoje['datetime'].time()
 
 with st.sidebar:
+	cols = st.columns([1,1])
+	cols[0].text(hoje['data'])
+	cols[1].text(hoje['hora'])
+	
 	with st.expander('usuario',expanded=True):
-		cols = st.columns([1,1])
-		cols[0].text(hoje['data'])
-		cols[1].text(hoje['hora'])
-
 		st.selectbox("usuarios",lst.usuarios,key='usuario')
 		st.write(st.session_state['usuario'])
+
+caminho =caminhos.tabelas['jcb_relatorio'] 
+st.write(caminho)
+
+df = extrair.gsheet_to_df(
+	id = caminho['id'],
+	tabela=caminhó['tabela'],
+	testar=False)
+
+st.dataframe(df)
+
 
 
 
---
+++ caminhos.py
@@ -0,0 +1,9 @@
+
+tabelas ={
+	'jcb_relatorio':{
+		'tipo':'gsheet',
+		'id':'1mzqMvNtH7tBgU4PZ78shFSgQrLT8RVU9RONczPbMKLQ',
+		'tabela':'Sheet1'},
+	}
+
+
