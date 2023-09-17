e1e6658f36a4679eb94c922f7ad70d7b47a5fbc0
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-17 09:04:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ hoje['data'] = hoje['datetime'].date()
 hoje['hora'] = hoje['datetime'].time()
 
 file = 'https://github.com/CAIOZANETTI/produtividade_maquina/blob/main/data/bronze_jcb_relatorio_2022.parquet'
+file = 'data/bronze_jcb_relatorio_2022.parquet'
 try:
 	df = pd.read_parquet(file,engine='pyarrow')
 
