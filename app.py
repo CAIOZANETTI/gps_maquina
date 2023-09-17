45bd5c0bde23dedb80784abed61d72f14a5eee9c
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-17 13:01:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -89,6 +89,7 @@ if st.session_state['relatorios']== 'filtros':
 			df2['cor'] = "#001eff"
 			df2['raio'] = 5
 			st.write(df2)
+			st.write(df_locais)
 
 			#st.map(df2, latitude='lat',longitude='lon',size='raio',color='cor')
 			#st.map(df_locais, latitude='lat',longitude='lon',size='raio',color='cor')	
