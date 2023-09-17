5e1f3e2f42d9bfff6b4889872e3172b37a652f22
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-17 11:19:27 -0300

    filtros data

--- filtros.py
+++ filtros.py
@@ -3,11 +3,8 @@ from datetime import datetime
 
 
 def df_periodo(df:pd.DataFrame,inicio,fim)->pd.DataFrame:
-	
-	if not isinstance(inicio, datetime):
-		inicio = datetime.strptime(inicio, "%Y-%m-%d")
-	if not isinstance(fim, datetime):
-		fim = datetime.strptime(fim, "%Y-%m-%d")
-	df1 = df[(df['data_hora']>= inicio) & (df['data_hora']<= fim)]
+	df['data'] = df['data_hora'].dt.date
+
+	df1 = df[(df['data']>= inicio) & (df['data']<= fim)]
 
 	return df1
