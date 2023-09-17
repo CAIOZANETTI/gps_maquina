4227f7e625a575180b68daed4e8f43dc319eb33b
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-17 11:37:52 -0300

    Update app.py
    ordenação de analise:
    filtrar...

--- app.py
+++ app.py
@@ -40,37 +40,36 @@ with st.sidebar:
 	#usuario 	
 	#st.selectbox("usuarios",lst.usuarios,key='usuario')
 	#st.write(st.session_state['usuario'])
-	st.radio("relatorios",['mapas','tabelas','graficos'],key='relatorios')
+	st.radio("relatorios",['filtros','calculos','futuro'],key='relatorios')
 
 with st.expander("df dataframe completo linhas:"+str(df.shape[0]), expanded=False):
 	st.dataframe(df)
 
-with st.expander("filtrar dataframe", expanded=True):
-	
-	cols = st.columns([1,1,1,1])
-	cols[0].text('periodo disponivel')
-	cols[0].text('jan/2022 -> ago/2022')
-	cols[1].date_input('inicio',datetime.datetime(2022,1,1),key='inicio')
-	cols[2].date_input('fim',datetime.datetime(2022,1,8),key='fim')
+if st.session_state['relatorios']== 'filtros':
+	with st.expander("filtrar dataframe", expanded=True):
+			
+		cols = st.columns([1,1,1,1])
+		cols[0].text('periodo disponivel')
+		cols[0].text('jan/2022 -> ago/2022')
+		cols[1].date_input('inicio',datetime.datetime(2022,1,1),key='inicio')
+		cols[2].date_input('fim',datetime.datetime(2022,1,8),key='fim')
 
-	
-	cols[3].text('ativar')
-	cols[3].button('filtros',key='btn_filtrar')
+		cols[3].text('ativar')
+		cols[3].button('filtros',key='btn_filtrar')
+		
+		if st.session_state['btn_filtrar']:
+			df1 = filtros.df_periodo(df,st.session_state['inicio'],st.session_state['fim'])
+		
+			with st.expander("**df1** dataframe filtrado linhas:"+str(df.shape[0]), expanded=False):
+				st.dataframe(df1)
 
-	
-	if st.session_state['btn_filtrar']:
-		df1 = filtros.df_periodo(df,st.session_state['inicio'],st.session_state['fim'])
-	
-		with st.expander("**df1** dataframe filtrado linhas:"+str(df.shape[0]), expanded=False):
-			st.dataframe(df1)
+			with st.expander('mapa', expanded=False):
+				st.write('mapas')
+				df2 = df1[['lat','lon']]
+				st.map(df2)
 
-		if st.session_state['relatorios']== 'mapas':
-			st.write('mapas')
-			df2 = df1[['lat','lon']]
-			st.map(df2)
+			with st.expander('tabelas', expanded=False):
+				st.write('tabelas')
 
-		if st.session_state['relatorios']== 'tabelas':
-			st.write('tabelas')
-
-		if st.session_state['relatorios']== 'graficos':
-			st.write('graficos')
+			with st.expander('graficos', expanded=False):
+				st.write('graficos')
