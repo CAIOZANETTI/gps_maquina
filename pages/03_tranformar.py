40a3e22d13c5d0a6df4cc1a325e56fa7a05c51ef
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-18 20:36:30 -0300

    Update 03_tranformar.py

--- pages/03_tranformar.py
+++ pages/03_tranformar.py
@@ -84,30 +84,5 @@ remover_colunas = ['id','hyperlink','maps_google_url']
 df = df_bronze_to_silver_gps(df=df,remover_colunas=remover_colunas)
 
 st.header("Tranformar - Silver")
-
-tab1,tab2,tab3,tab4,tab5 = st.tabs(['dataframe','describe','shape','columns','bytes'])
-
-with tab1:
-	st.dataframe(df)
-
-with tab2:
-	st.write(df.describe())
-
-with tab3:
-	shape = {}
-	shape['rows'] = df.shape[0]
-	shape['columns'] = df.shape[1]
-	st.write(shape)
-
-with tab4:
-	st.write(df.dtypes)
-
-with tab5:
-	memory = {}
-	memory['bytes'] = int(df.memory_usage(deep=True).sum())
-	memory['Kb'] = round(memory['bytes'] / 1024,2)
-	memory['Mb'] = round(memory['bytes'] / 1048576,2)
-	
-	st.write(memory)
-
+fx_streamlit.analise_df(df)
 st.session_state['df1'] = df
