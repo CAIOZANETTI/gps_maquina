d0ad2cd844fdf0f177d2cfa24b9bab29f89bae62
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-18 20:51:34 -0300

    Update 03_tranformar.py

--- pages/03_tranformar.py
+++ pages/03_tranformar.py
@@ -82,7 +82,7 @@ def df_bronze_to_silver_gps(remover_colunas:list,df)->pd.DataFrame:
 
 df = st.session_state['df']
 remover_colunas = ['id','hyperlink','maps_google_url']
-#df = df_bronze_to_silver_gps(df=df,remover_colunas=remover_colunas)
+df = df_bronze_to_silver_gps(df=df,remover_colunas=remover_colunas)
 
 st.header("Tranformar - Silver")
 fx_streamlit.analise_df(df)
