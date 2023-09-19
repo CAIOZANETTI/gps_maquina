0a74b1bc88e5cb1b2fe91f3c5b2382c4cafe1f15
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-18 22:00:21 -0300

    Update 03_tranformar.py

--- pages/03_tranformar.py
+++ pages/03_tranformar.py
@@ -73,7 +73,7 @@ def df_bronze_to_silver_gps(remover_colunas:list,df)->pd.DataFrame:
 	colunas_string ={'nome_dia': 'string','lat_lon':'string','atividade':'string'}
 	st.write('teste 01')
 	for coluna in colunas_string:
-		df.astype(colunas_str)#.dtypes
+		df.astype(coluna)#.dtypes
 	st.write('teste 02')
 	#remover colunas
 	for coluna in remover_colunas:
