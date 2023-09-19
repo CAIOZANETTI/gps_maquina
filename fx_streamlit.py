3240102717a656bb8218c38e42b9fd7b20790fb4
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-18 22:09:23 -0300

    Update fx_streamlit.py

--- fx_streamlit.py
+++ fx_streamlit.py
@@ -4,7 +4,7 @@ def analise_df(df,nome):
 
 	st.subheader(nome)
 
-	tab1,tab2,tab3,tab4,tab5 = st.tabs(['1-dtypes','2-dataframe','3-shape','4-describe','5-bytes'])
+	tab1,tab2,tab3,tab4,tab5,tab6 = st.tabs(['dtypes','dataframe','table','shape','describe','bytes'])
 
 	with tab1:
 		st.write(df.dtypes)
@@ -13,15 +13,18 @@ def analise_df(df,nome):
 		st.dataframe(df)
 
 	with tab3:
+		st.table(df)
+
+	with tab4:
 		shape = {}
 		shape['rows'] = df.shape[0]
 		shape['columns'] = df.shape[1]
 		st.write(shape)
 
-	with tab4:
+	with tab5:
 		st.write(df.describe())
 
-	with tab5:
+	with tab6:
 		memory = {}
 		memory['bytes'] = int(df.memory_usage(deep=True).sum())
 		memory['Kb'] = round(memory['bytes'] / 1024,2)
