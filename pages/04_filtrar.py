edce81c05130649e7162f5e53fa4ce97e3e504b1
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-19 20:20:36 -0300

    filtrar.py
    incluir filtros

--- pages/04_filtrar.py
+++ pages/04_filtrar.py
@@ -1,8 +1,15 @@
 import datetime
 import streamlit as st
+from datetime import datetime
+
+
+def df_filtrar_datas(df:pd.DataFrame,inicio,fim)->pd.DataFrame:
+	df['data'] = df['data_hora'].dt.date
+	df1 = df[(df['data']>= inicio) & (df['data']<= fim)]
+	return df1
 
 st.subheader("Filtrar")
-
+df1 = st.session_state['df1']
 tab1,tab2,tab3,tab4 = st.tabs(['periodo','atividades principais','todas atividades','3-dia da semana'])
 
 with tab1:
@@ -13,10 +20,21 @@ with tab1:
 
 	dias = st.session_state['fim'] - st.session_state['inicio']
 	dias = dias.days
+
+
 	if dias>0:
+		cols = st.columns([1,1])
 		st.write('Periodo: '+str(dias)+' dias')
+		st.button('filtrar_datas',key=filtrar_datas)
+
 	elif dias<0:
 		st.write('NEGATIVO REVISAR '+str(dias)+' dias')
+
+	df2 = df1
+	if st.session_state['filtrar_datas']==True:
+		df2 = df_filtrar_datas(df1,
+			inicio=st.session_state['inicio'],
+			fim = st.session_state['fim'])
 
 with tab2:
 	st.write('atividades principais')
@@ -32,10 +50,14 @@ st.subheader("Filtro Aplicado Dataframe")
 tab1,tab2,tab3 = st.tabs(['dataframe','mapa','dias'])
 
 with tab1:
-	st.write('atividades principais')
+	st.dataframe(df2)
 
 with tab2:
-	st.write('todas atividades')
+	df_map = df1[['lat','lon']]
+	df_map['cor'] = "#001eff"
+	df_map['raio'] = 5
+	st.map(df_map, latitude='lat',longitude='lon',size='raio',color='cor')
+
 
 with tab3:
 	st.write('todas atividades')
---
+++ pages/05_entendimento.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.subheader("Entendimento")
+
+st.write(
+'A compreender as informações dos dados\
+identificar quais informações são relevantes,\
+como elas estão distribuídas, qual a relação\
+entre demais variáveis como o tempo (hora, dia, mês)\
+ou coordenadas lat+lon.')
+
+# atividades....
+#tab1,tab2,tab3,tab4 = st.tabs(['atividade_unicas','dias'])
