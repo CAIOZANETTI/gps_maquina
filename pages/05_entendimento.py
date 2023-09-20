d27d8bb137793ec285e4b0fae94994adcdbf2eab
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-20 20:05:30 -0300

    Update 05_entendimento.py

--- pages/05_entendimento.py
+++ pages/05_entendimento.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.subheader("Entendimento")
 
@@ -11,3 +12,24 @@ ou coordenadas lat+lon.')
 
 # atividades....
 #tab1,tab2,tab3,tab4 = st.tabs(['atividade_unicas','dias'])
+
+
+data_df = pd.DataFrame(
+    {
+        "sales": [200, 550, 1000, 80],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.ProgressColumn(
+            "Sales volume",
+            help="The sales volume in USD",
+            format="$%f",
+            min_value=0,
+            max_value=1000,
+        ),
+    },
+    hide_index=True,
+)
