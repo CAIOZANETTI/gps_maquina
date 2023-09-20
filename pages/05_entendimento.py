e2a3b720a33ebad376bab5915cb8e2e52cf5d7c6
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-20 20:52:15 -0300

    Update 05_entendimento.py

--- pages/05_entendimento.py
+++ pages/05_entendimento.py
@@ -49,7 +49,7 @@ with tab1: #periodo
 with tab2: #periodo
 
 	st.write('atividades vs horas')
-	df3 = df1.pivot_table(index='atividade', columns='horas', aggfunc='sum', fill_value=0)
+	df3 = df1.pivot_table(index='atividade', columns='horas')#, aggfunc='sum', fill_value=0)
 	
 	st.write(df3)
 
