a776dd978cf0de5e8c86f7368ef5db685d028d1a
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-20 20:45:40 -0300

    Update 05_entendimento.py

--- pages/05_entendimento.py
+++ pages/05_entendimento.py
@@ -20,35 +20,34 @@ if 'df1' not in st.session_state:
 df1 = st.session_state['df1']
 
 
-st.dataframe(df1.head(2))
-
-df2 = df1['atividade'].value_counts().reset_index()
-df2.columns = ['atividade','qtd']
-qtd_min = int(round(df2['qtd'].min(),0))
-qtd_max = int(round(df2['qtd'].max(),0))
-
-
-#st.write(qtd_min,type(qtd_min))
-
-
-st.dataframe(df2.head(2))
-
-#st.write(df2.describe())
-
-
-st.data_editor(
-    df2,
-    column_config={
-        "qtd": st.column_config.ProgressColumn(
-            "quantity",
-            width='large',
-            format='%f',
-            min_value=qtd_min,
-            max_value=qtd_max,
-        ),
-    },
-    hide_index=True,
-)
 
 
 
+tab1,tab2,tab3 = st.tabs(['atividades','atividades vs horas',' atividades vs dia da semana'])
+
+with tab1: #periodo
+	df2 = df1['atividade'].value_counts().reset_index()
+	df2.columns = ['atividade','qtd']
+	qtd_min = int(round(df2['qtd'].min(),0))
+	qtd_max = int(round(df2['qtd'].max(),0))
+	
+	st.data_editor(
+	    df2,
+	    column_config={
+	        "qtd": st.column_config.ProgressColumn(
+	            "quantity",
+	            width='large',
+	            format='%f',
+	            min_value=qtd_min,
+	            max_value=qtd_max,
+	        ),
+	    },
+	    hide_index=True,
+	)
+
+
+with tab2: #periodo
+	st.write('atividades vs horas')
+
+with tab3: #periodo
+	st.write(' atividades vs dia da semana')
