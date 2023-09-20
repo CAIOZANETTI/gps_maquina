79c526f5a4601c511719370f480f3240f5e25e0a
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-20 20:32:46 -0300

    Update 05_entendimento.py

--- pages/05_entendimento.py
+++ pages/05_entendimento.py
@@ -44,7 +44,7 @@ st.dataframe(df1.head(2))
 df2 = df1['atividade'].value_counts().reset_index()
 df2.columns = ['atividade','qtd']
 qtd_max = int(round(df2['qtd'].max(),0))
-qtd_min = round(df2['qtd'].min(),0)
+qtd_min = int(round(df2['qtd'].min(),0))
 
 st.write(qtd_min,type(qtd_min))
 
