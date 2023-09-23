b2a6329e193244ea34b1452c5fd633f8ee0fe7f8
CAIOZANETTI <caiozanetti@gmail.com>
2023-09-23 15:07:58 -0300

    Update 01_readme.py

--- pages/01_readme.py
+++ pages/01_readme.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.header("Readme")
+
+def show_readme(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        contents = file.read()
+        st.markdown(contents)
+
+# Path to your readme.md file
+readme_path = 'readme.md'
+
+# Streamlit app
+st.title('Readme Viewer')
+
+# Display the contents of the readme.md file
+show_readme(readme_path)
