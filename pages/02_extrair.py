ce7258899c86074c306b83079e4fb90c98232304
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-02 08:20:56 -0300

    Update 02_extrair.py

--- pages/02_extrair.py
+++ pages/02_extrair.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import fx_streamlit as fx_streamlit
 
-st.header("Extração BRONZE")
+st.header("Extração RAW->BRONZE")
 
 files = [
 	#'data/silver_jcb_relatorio_2022.parquet',
@@ -17,7 +17,11 @@ try:
 except:
 	df = pd.read_parquet(st.session_state['file'],engine='fastparquet')
 
-fx_streamlit.analise_df(df,'bronze ....')
+with st.expander('info',expanded=False):
+	st.write('Primeira extração dos dados de como foi disponibilizado,\
+		sem nenhuma modificação os dados estão em formato original (RAW),\
+		essa primeira etapa apenas tranforma em formato dataframe pandas\
+		o resultado pode ser verificado nas abas de info sobre o dataframe')
 
+fx_streamlit.analise_df(df,'raw-> bronze')
 st.session_state['df'] = df
-st.write('teste')
