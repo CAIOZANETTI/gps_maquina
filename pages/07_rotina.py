4352283fb3a732204b7605c0acc5df812ea1ed53
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-03 08:58:39 -0300

    Update 07_rotina.py

--- pages/07_rotina.py
+++ pages/07_rotina.py
@@ -21,8 +21,7 @@ with tab1:
 	filtro = 'motor_ligado==True'
 	cols[0].write(filtro)
 	df2 = df1.query(filtro)
-	df2 = df2['nome_dia'].value_counts()
-	st.dataframe(df2.T)
+	cols[0].dataframe(df2['nome_dia'].value_counts())
 	
 	filtro = 'motor_ligado==False'
 	cols[1].write(filtro)
