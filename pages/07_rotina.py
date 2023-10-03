fa89fd367dc6f36b61ed772126b4dca4d125ff7b
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-03 08:53:22 -0300

    Update 07_rotina.py

--- pages/07_rotina.py
+++ pages/07_rotina.py
@@ -12,7 +12,7 @@ with st.expander('info',expanded=False):
 	st.markdown('analisar a rotina da maqiona no periodo disponivel no dataframe,\
 	 afim de identificar algum padrão que possa ser caracterizado como um comportamento')
 
-tab1,tab2,tab3,tab4,tab5 = st.tabs(['semana','horas','inicio','termino'])
+tab1,tab2,tab3,tab4,tab5 = st.tabs(['semana','horas','deslocamento','inicio','termino'])
 
 
 with tab1:
