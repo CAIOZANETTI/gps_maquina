408a2ad67556d74b57d06d95a14d5167e8d95ae1
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-06 13:58:34 -0300

    entendimento

---
+++ pages/04_entendimento.py
@@ -0,0 +1,22 @@
+import datetime
+import streamlit as st
+import pandas as pd
+#from datetime import datetime
+
+import fx_streamlit as fx_streamlit
+
+
+#carregar df1
+if 'df1' not in st.session_state:
+	df1 = pd.read_parquet('data/silver_jcb_relatorio_2022.parquet',engine='pyarrow')
+	st.session_state['df1'] = df1
+df1 = st.session_state['df1']
+
+
+with st.expander('periodo disponivel no dataframe',expanded=False):
+
+	cols = st.columns([1,1])
+	cols[0].write('inicio: ' +str(df['data'].min()))
+	cols[1].write('fim: ' +str(df['data'].min()))
+
+	
