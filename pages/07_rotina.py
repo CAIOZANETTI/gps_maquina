534d7f4d85e5986adc4fe8c09e832fc02ca88913
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-06 10:44:01 -0300

    Update 07_rotina.py

--- pages/07_rotina.py
+++ pages/07_rotina.py
@@ -73,10 +73,10 @@ with tab2:
 		df2= df1.query(filtro)
 		df2 = df2['hora'].value_counts().reset_index()
 
-		#ordenar index
-		#ordem_index =  list(range(0, 23))
-		#df2.set_index('hora',inplace=True)
-		#df2 = df2.reindex(ordem_index)
+		ordenar index
+		ordem_index =  list(range(0, 23))
+		df2.set_index('hora',inplace=True)
+		df2 = df2.reindex(ordem_index)
 
 		st.dataframe(df2.T)
 
