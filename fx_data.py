2d0df233eb5eacc6c99d8eb05b88c95353f5e0b9
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-10 08:16:45 -0300

    Update fx_data.py

--- fx_data.py
+++ fx_data.py
@@ -202,8 +202,8 @@ class PeriodoDataFrame:
 		df['dia_util'] = True
 		df.loc[df.index.isin(['saturday', 'sunday']), 'dia_util'] = False
 
-		self.qtd_med_weekdays = int(df['qtd'].median())# retornar somente parte inteira
-		self.std_med_weekdays = round(df['qtd'].std(),2)
+		self.qtd_med_weekdays = round(df['qtd'].median(),2)# retornar somente parte inteira
+		self.std_med_weekdays = -round(df['qtd'].std(),3)
 
 
 		return df
