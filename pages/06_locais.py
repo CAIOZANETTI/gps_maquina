fbb04f29fcb8dc0a2f6e8e5563d7f6c564a83d87
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-11 11:19:51 -0300

    Update 06_locais.py

--- pages/06_locais.py
+++ pages/06_locais.py
@@ -9,12 +9,11 @@ import fx_streamlit as fx_streamlit
 if 'df1' not in st.session_state:
 	df1 = pd.read_parquet('data/silver_jcb_relatorio_2022.parquet',engine='pyarrow')
 	st.session_state['df1'] = df1
-
 df1 = st.session_state['df1']
 
-st.write('ainda esta vazio !!!')
+st.subheader('Mapa de Localização')
 
-with st.expander('localização com maior incidencia', expanded=True)
+with st.expander('localização com maior incidencia', expanded=True):
 	
 	df_map = df1[['lat','lon']].describe()
 	st.dataframe(df_map.T)
