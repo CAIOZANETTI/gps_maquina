079f03bb75f2c4af965d78fbc37046f6171a1112
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-11 11:32:51 -0300

    Update 06_locais.py

--- pages/06_locais.py
+++ pages/06_locais.py
@@ -17,11 +17,14 @@ with st.expander('localização com maior incidencia', expanded=True):
 	
 	df_map = df1[['lat','lon']].describe()
 	df_map = df_map.reset_index()
-	st.dataframe(df_map)
-	st.dataframe(df_map)
+	
+	df_map_relev = df_map.query('index=="50%" and index=="75%" and index=="max"')
+	st.dataframe(df_map_relev)
 
-	st.write(df_map.loc['75%'])
-	st.write(df_map.loc['max'].T)
-	#st.map(df_map.loc['75%'].T)
+
+
+	#st.write(df_map[''.loc['75%'])
+	#st.write(df_map.loc['max'].T)
+	st.map(df_map_relev)
 
 
