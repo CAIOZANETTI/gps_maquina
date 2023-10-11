1020f6ad9a0f361f79ca408877a41d2666af5c9f
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-11 07:54:57 -0300

    Update 07_rotina.py

--- pages/07_rotina.py
+++ pages/07_rotina.py
@@ -18,7 +18,7 @@ with st.expander('Chave **Ligada**',expanded=False):
 	filtro = 'atividade == "chave_ligada"'
 	st.write(filtro)
 
-tab1,tab2,tab3,tab4,tab5 = st.tabs(['periodo','dias chave_on','horas chave_on','inicio','termino'])
+tab1,tab2,tab3,tab4,tab5 = st.tabs(['vazio','dia','hora','inicio','termino'])
 
 with tab1:
 	st.write('vazio')
@@ -48,7 +48,7 @@ with tab2: #dias chave_on
 		df3['chave_on_dia'] = round(df3['chave_on_total']/df3['qtd'],2)
 		st.dataframe(df3)
 	
-	with st.expander('Quantidade **Média** acionamento da Maquina por **dia**', expanded=False):
+	with st.expander('Grafico e tabela de **Média**', expanded=False):
 		cols = st.columns([1,2])
 		cols[0].dataframe(df3['chave_on_dia'])
 		cols[1].bar_chart(df3['chave_on_dia'])
@@ -61,8 +61,6 @@ with tab2: #dias chave_on
 		med_chave_on_dia = int(df4['chave_on_dia'].median())
 		variacao = round(df4['chave_on_dia'].median()-med_chave_on_dia,2)
 		st.metric('Chave_on **(Ligado)**',str(med_chave_on_dia)+'x  /dia',variacao)
-
-
 
 with tab3:
 	with st.expander('qtd **Total** chave_on por **hora**', expanded=True):
@@ -77,33 +75,21 @@ with tab3:
 		#ordenar index
 		ordem_index =  list(range(0, 23))
 		df2.set_index('hora',inplace=True)
+		
 		df2 = df2.reindex(ordem_index)
 
 		st.dataframe(df2.T)
 
 	with st.expander('qtd **dias uteis**', expanded=True):
-		df2 = df1.query(filtro)
-		df_data = df2['data'].unique()
-		st.write(df_data.shape)
-		st.write(df_data)
-
-		st.write('qtd **dias uteis** no periodo = '+str(160))
+		st.write('vazio')
 
 	with st.expander('qtd **Média** chave_on por **hora**', expanded=True):	
-		
-		df3 = pd.DataFrame()
-		df3 = round(df2/media_dia_util,0)
-		st.dataframe(df3.T)
+		st.write('vazio')
 	
 with tab4:
 	st.write('primeira hora do dia que a chave ligou')
-	#inicio = df1[df1['atividade'] == 'chave_ligada']['hora'].iloc[0]
-	#st.dataframe(inicio)
+	
 
 with tab5:
 	st.write('ultima hora hora do dia que a chave desligou')
 
-
-#filtro = 'motor_ligado==False'
-#st.write('Improdutiva: '+filtro)
-#df2 = df1.query(filtro)
