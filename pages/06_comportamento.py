591e87b75583cc050c25ed99bc4cd81c3aefa57c
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-11 17:13:40 -0300

    Update 06_comportamento.py

--- pages/06_comportamento.py
+++ pages/06_comportamento.py
@@ -18,10 +18,77 @@ ano_mes_unico = df1['ano_mes'].unique()
 st.selectbox('selcionar ano mes',ano_mes_unico,key='ano_mes')
 df1 = df1[df1['ano_mes'] ==st.session_state['ano_mes']]
 
-tab1,tab2,tab3,tab4,tab5 = st.tabs(['improvavel','madrugada','manhã','tarde','noite'])
-
+tab1,tab2,tab3,tab4,tab5 = st.tabs(['madrugada','manhã','tarde','noite','improvavel'])
 
 with tab1:
+	filtro = [
+		'(hora>0) and (hora<6)',
+		'(motor_ligado==True) and (hora>0) and (hora<6)'
+		]
+	filtro_ativo = st.selectbox('filtrar as condições:',filtro)
+
+	with st.expander('dataframe', expanded=False):
+		df2 = df1.query(filtro_ativo)
+		st.dataframe(df2)
+
+	with st.expander('Mapa', expanded=True):
+		st.map(df2[['lat','lon']],size=5)
+
+	with st.expander('informação do dataframe', expanded=False):
+		fx_streamlit.analise_df(df2,filtro_ativo)
+
+with tab2:
+	filtro = [
+		'(hora>6) and (hora<12)',
+		'(motor_ligado==True) and (hora>6) and (hora<12)'
+		]
+	filtro_ativo = st.selectbox('filtrar as condições:',filtro)
+
+	with st.expander('dataframe', expanded=False):
+		df2 = df1.query(filtro_ativo)
+		st.dataframe(df2)
+
+	with st.expander('Mapa', expanded=True):
+		st.map(df2[['lat','lon']],size=5)
+	
+	with st.expander('informação do dataframe', expanded=False):
+		fx_streamlit.analise_df(df2,filtro_ativo)
+
+with tab3:
+	filtro = [
+		'(hora>12) and (hora<18)',
+		'(motor_ligado==True) and (hora>12) and (hora<18)'
+		]
+	filtro_ativo = st.selectbox('filtrar as condições:',filtro)
+
+	with st.expander('dataframe', expanded=False):
+		df2 = df1.query(filtro_ativo)
+		st.dataframe(df2)
+
+	with st.expander('Mapa', expanded=True):
+		st.map(df2[['lat','lon']],size=5)
+
+	with st.expander('informação do dataframe', expanded=False):
+		fx_streamlit.analise_df(df2,filtro_ativo)
+
+with tab4:
+	filtro = [
+		'(hora>18)',
+		'(motor_ligado==True) and (hora>18)'
+		]
+	filtro_ativo = st.selectbox('filtrar as condições:',filtro)
+
+	with st.expander('dataframe', expanded=False):
+		df2 = df1.query(filtro_ativo)
+		st.dataframe(df2)
+
+	with st.expander('Mapa', expanded=True):
+		st.map(df2[['lat','lon']],size=5)
+
+	with st.expander('informação do dataframe', expanded=False):
+		fx_streamlit.analise_df(df2,filtro_ativo)
+
+with tab5:
 	
 	with st.expander('motor foi ligado em dia de **folga**', expanded=True):
 		filtro = [
@@ -35,76 +102,7 @@ with tab1:
 		st.dataframe(df2)
 
 	with st.expander('Mapa', expanded=True):
-		st.map(df2[['lat','lon']])
+		st.map(df2[['lat','lon']],size=5)
 
 	with st.expander('informação do dataframe', expanded=False):
 		fx_streamlit.analise_df(df2,filtro_ativo)
-
-with tab2:
-	filtro = [
-		'(hora>0) and (hora<6)',
-		'(motor_ligado==True) and (hora>0) and (hora<6)'
-		]
-	filtro_ativo = st.selectbox('filtrar as condições:',filtro)
-
-	with st.expander('dataframe', expanded=False):
-		df2 = df1.query(filtro_ativo)
-		st.dataframe(df2)
-
-	with st.expander('Mapa', expanded=True):
-		st.map(df2[['lat','lon']])
-
-	with st.expander('informação do dataframe', expanded=False):
-		fx_streamlit.analise_df(df2,filtro_ativo)
-
-with tab3:
-	filtro = [
-		'(hora>6) and (hora<12)',
-		'(motor_ligado==True) and (hora>6) and (hora<12)'
-		]
-	filtro_ativo = st.selectbox('filtrar as condições:',filtro)
-
-	with st.expander('dataframe', expanded=False):
-		df2 = df1.query(filtro_ativo)
-		st.dataframe(df2)
-
-	with st.expander('Mapa', expanded=True):
-		st.map(df2[['lat','lon']])
-	
-	with st.expander('informação do dataframe', expanded=False):
-		fx_streamlit.analise_df(df2,filtro_ativo)
-
-with tab4:
-	filtro = [
-		'(hora>12) and (hora<18)',
-		'(motor_ligado==True) and (hora>12) and (hora<18)'
-		]
-	filtro_ativo = st.selectbox('filtrar as condições:',filtro)
-
-	with st.expander('dataframe', expanded=False):
-		df2 = df1.query(filtro_ativo)
-		st.dataframe(df2)
-
-	with st.expander('Mapa', expanded=True):
-		st.map(df2[['lat','lon']])
-
-	with st.expander('informação do dataframe', expanded=False):
-		fx_streamlit.analise_df(df2,filtro_ativo)
-
-with tab5:
-	filtro = [
-		'(hora>18)',
-		'(motor_ligado==True) and (hora>18)'
-		]
-	filtro_ativo = st.selectbox('filtrar as condições:',filtro)
-
-	with st.expander('dataframe', expanded=False):
-		df2 = df1.query(filtro_ativo)
-		st.dataframe(df2)
-
-	with st.expander('Mapa', expanded=True):
-		st.map(df2[['lat','lon']])
-
-	with st.expander('informação do dataframe', expanded=False):
-		fx_streamlit.analise_df(df2,filtro_ativo)
-
